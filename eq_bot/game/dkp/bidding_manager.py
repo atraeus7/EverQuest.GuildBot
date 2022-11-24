11c8db22aa28ed3553b713f93da0829af70027ff
jmvogt <jordanmvogt@gmail.com>
2022-11-24 05:20:13 -0800

    track multiple instances of same item

--- eq_bot/game/dkp/bidding_manager.py
+++ eq_bot/game/dkp/bidding_manager.py
@@ -8,17 +8,23 @@ RESTRICT_TO_GUILDIES = get_config('dkp.bidding.restrict_to_guildies', True)
 
 MAX_GUILD_MESSAGE_LENGTH = 508
 
-class Bid:
-    def __init__(self):
-        pass
 
+class BiddableItem:
+    def __init__(self, name):
+        self.count = 1
+        self.name = name
+    
+    def increase_count(self):
+        self.count += 1
 
+    def print(self):
+        return self.name if self.count == 1 else f'{self.name} x{self.count}'
 
 class BiddingManager:
     def __init__(self, eq_window: EverQuestWindow, guild_tracker: GuildTracker):
         self._eq_window = eq_window
         self._guild_tracker = guild_tracker
-        self._next_round = {}
+        self._next_round_items = []
         self._round_enabled = False
 
     def handle_tell_message(self, tell_message):
@@ -42,11 +48,16 @@ class BiddingManager:
                 return
 
             for item in bid_message.items:
-                self._next_round[item] = {}
+                existing_item = next((i for i in self._next_round_items if i.name == item), None)
+                if existing_item:
+                    existing_item.increase_count()
+                else:
+                    self._next_round_items.append(BiddableItem(item))
+
             print('BID MESSAGE PARSED!')
+
         if bid_message.message_type == BidMessageType.START_ROUND:
-            next_round_keys = list(self._next_round.keys())
-            if len(next_round_keys) == 0:
+            if len(self._next_round_items) == 0:
                 print('Received start round message, but no items are in the next round.')
                 self._eq_window.send_tell_message(
                     bid_message.from_player,
@@ -56,16 +67,17 @@ class BiddingManager:
             self._round_enabled = True
 
             messages_to_send = []
-            guild_message = f'BIDDING CURRENTLY OPEN ON: {next_round_keys[0]}'
+            guild_message = f'BIDDING CURRENTLY OPEN ON: {self._next_round_items[0].print()}'
 
-            for item in next_round_keys[1:]:
-                if len(guild_message) + len(item) + 2 <= MAX_GUILD_MESSAGE_LENGTH:
-                    guild_message += f', {item}'
+            for item in self._next_round_items[1:]:
+                item_message = item.print()
+                if len(guild_message) + len(item_message) + 2 <= MAX_GUILD_MESSAGE_LENGTH:
+                    guild_message += f', {item_message}'
                 else:
                     messages_to_send.append(guild_message)
 
                     # start the next message with the current item
-                    guild_message = item
+                    guild_message = item_message
             
             messages_to_send.append(guild_message)
 
@@ -74,4 +86,4 @@ class BiddingManager:
                     bid_message.from_player,
                     message)
 
-        print('ITEMS IN NEXT ROUND', self._next_round)
+        print('ITEMS IN NEXT ROUND', self._next_round_items)
