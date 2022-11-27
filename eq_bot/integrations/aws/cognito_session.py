1a4408ef5d74f9fc6e4918262c07002b0db6c5cd
jmvogt <jordanmvogt@gmail.com>
2022-11-27 01:55:40 -0800

    Cleanup cognito session

--- eq_bot/integrations/aws/cognito_session.py
+++ eq_bot/integrations/aws/cognito_session.py
@@ -45,7 +45,7 @@ class CognitoSession:
                 f'cognito-idp.{self._region}.amazonaws.com/{self._user_pool}':
                     self._tokens.id_token
             })
-        print('RESPONSE', response, 'CREDS THO', response['Credentials']['Expiration'])
+
         return IamCredentials(
             expires_at = response['Credentials']['Expiration'],
             access_key_id = response['Credentials']['AccessKeyId'],
@@ -59,6 +59,7 @@ class CognitoSession:
             pool_id=self._user_pool,
             client_id=self._client_id,
             pool_region=self._region).authenticate_user()
+
         return CognitoCredentials(
             # Remove 10 seconds just in case there was latency when returning response
             expires_at=local_datetime() + timedelta(seconds=tokens['AuthenticationResult']['ExpiresIn'] - 10),
@@ -68,10 +69,7 @@ class CognitoSession:
     
     @property
     def tokens(self):
-        # TODO: Check if cognito credentials are expired
-        # and refresh if so
-        if self._tokens is not None:
-            print('HAY', local_datetime(), self._tokens.expires_at)
+        # TODO: Check/refresh cognito credentials if they are expired
         if not self._tokens or local_datetime() >= self._tokens.expires_at:
             self._tokens = self._refresh_tokens()
         return self._tokens
@@ -84,8 +82,7 @@ class CognitoSession:
 
     @property
     def iam_credentials(self):
-        if self._iam_credentials is not None:
-            print('HAY', local_datetime(), self._iam_credentials.expires_at)
+        # TODO: Check/refresh IAM credentials if they are expired
         if not self._iam_credentials or local_datetime() >= self._iam_credentials.expires_at:
             self._iam_credentials = self._get_iam_credentials()
         return self._iam_credentials
--- eq_bot/integrations/opendkp/opendkp.py
+++ eq_bot/integrations/opendkp/opendkp.py
@@ -6,8 +6,11 @@ class OpenDkp:
     def __init__(self):
         self._api_gateway = OpenDkpApiGateway()
 
+    # Example flow of how we can integrate/push changes to OpenDKP directly
     def create_raid(self, raid_name):
-        return self._api_gateway.create_raid(raid_name)
+        # TODO: Lookup raid by name and ensure it doesn't exist, or else
+        # we will create a duplicate raid with an identical name
+        print(self._api_gateway.create_raid(raid_name))
 
     def get_dkp_summary(self) -> DkpSummary:
         return self._api_gateway.fetch_dkp_summary()
